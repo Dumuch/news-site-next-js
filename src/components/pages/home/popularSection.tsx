708592a6c25b24f45de02d6c7a347a924bf32e42
kizin_d <dumuch@yandex.ru>
2023-08-17 13:19:15 +0800

    Update popularSection.tsx

--- src/components/pages/home/popularSection.tsx
+++ src/components/pages/home/popularSection.tsx
@@ -7,11 +7,30 @@ import { FC } from 'react';
 interface props {
     articles: ArticleInterface[];
 }
+
+const responsiveOptions = [
+    {
+        breakpoint: '1199px',
+        numVisible: 3,
+        numScroll: 1
+    },
+    {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+    },
+    {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+    }
+];
+
 export const PopularArticlesSection: FC<props> = ({ articles }) => {
     return (
         <section>
             <TitleSection title={'Popular Articles'} />
-            <Carousel value={articles} numVisible={3} numScroll={3} itemTemplate={PreviewArticleMin} />
+            <Carousel value={articles} numVisible={4} responsiveOptions={responsiveOptions} numScroll={1} itemTemplate={PreviewArticleMin} />
         </section>
     );
 };
